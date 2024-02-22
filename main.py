04e2a93e8bb541f58a2fd78a465b15b9b2b636ff
LE_428 <k-c.f@t-online.de>
2024-02-22 12:06:47 +0100

    changed main in merge branch

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}, nice to meet you')  # Press Strg+F8 to toggle the breakpoint.
     print('So I did this in Python')
+    print('git is gamechanging!')
 
 
 # Press the green button in the gutter to run the script.
